435cef7955976cb7e421f7d89586f4433821fb8c
Antiokus314 <nrgarg@outlook.com>
2015-02-16 13:44:20 -0600

    Pre-commit. Need to check if this works before pushing

--- index.js
+++ index.js
@@ -27,21 +27,21 @@ function load(program, _, path, Generator) {
     .description('Start your propel application')
     .option("-p, --prod, Run application in production mode. Starts static file server serving out of `build` directory")
     .action(function(args) {
-//      loadApp().run({ production: args.prod || false });
+      loadApp().run({ production: args.prod || false });
     });
 
   program
     .command('build')
     .description('Build your project')
     .action(function() {
-//      loadApp().build();
+      loadApp().build();
     });
 
   program
     .command('clean')
     .description('Clean your .tmp and build directories')
     .action(function() {
-//      loadApp().clean();
+      loadApp().clean();
     });
 
   program
@@ -49,35 +49,45 @@ function load(program, _, path, Generator) {
     .description('Package the build up for distribution')
     .option("-c, --config-file <config-file>")
     .option("-p, --properties [properties...]")
+    .option("-n --no-interactive")
     .on('--help', function() {
       console.log('HELP');
       console.log('----');
+      console.log('    pkg -n                 Disable interactive terminal session. Flag to allow below options');
       console.log('    pkg -c <config-file>   Read from config file. Defaults to application.js configuration');
-      console.log('    pkg -p [properties...] Indicate properties to read from in config file (used to create final tar ball). Defaults to application.js configuration');
+      console.log('    pkg -p [properties...] Indicate properties to read from config file (comma separated with no spaces)\n' +
+                  '                           used to create final tar ball. Defaults to application.js configuration');
+      console.log('');
+      console.log('    If no options are provided interactive session will start');
       console.log('');
     })
     .action(function(args) {
-//      loadApp().pkg({ configFile: args.configFile, fileProps: args.properties });
+      loadApp().pkg({ configFile: args.configFile, fileProps: args.properties, interactive: args.noInteractive === undefined });
     });
 
   program
     .command('bump')
     .description('Bump or reset version numbers for specified')
+    .option('-n, --no-interactive')
     .option("-f, --files [version-files...]")
     .option("-t, --type <bump-type>")
     .option("-c, --custom <custom>")
     .on('--help', function() {
       console.log('HELP');
       console.log('----');
-      console.log('    pkg -f [version-files...] Supply list of files (comma separated string with no spaces) to apply version bump. Defaults to application.json configuration');
-      console.log('    pkg -t <bump-type>        Bump files to specified bump type. Options: major, minor, patch');
+      console.log('    pkg -n                    Disable interactive terminal session. Flag to allow below options');
+      console.log('    pkg -f [version-files...] Supply list of files (comma separated string with no spaces) to apply version bump.\n' +
+                  '                              Defaults to application.js configuration');
+      console.log('    pkg -t <bump-type>        Bump files to specified bump type. Options: major, minor, patch. Defaults to application.js configuration');
       console.log('    pkg -c <custom>           Use custom version number to apply to all given files');
+      console.log('');
+      console.log('    If no options are provided interactive session will start    ');
       console.log('');
     })
     .action(function(args) {
-//      loadApp().bump({ files: args.versionFiles, type: args.bumpType, custom: args.custom });
+      loadApp().bump({ files: args.versionFiles, type: args.bumpType, custom: args.custom, interactive: args.noInteractive === undefined });
     });
 
 
-  return program.parse(process.argv);
+  return program;
 }
