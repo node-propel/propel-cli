330e11f1b9664eafb2d6265cbfa07acd33dca9ae
Antiokus314 <navneetgarg123@gmail.com>
2015-11-26 16:51:52 -0500

    Simplify cli usage
      1) Remove existing generator & templates
      2) Default all installations to master branch

--- index.js
+++ index.js
@@ -1,102 +1,72 @@
-exports = module.exports = load(require('commander'),
-                                require('lodash'),
-                                require('path'),
-                                require('./lib/generator'));
+var program = require('commander');
+var shell = require('shelljs');
+var version = require('./package').version;
+var path = require('path');
+var fs = require('fs');
+var expressURL = "git@github.com:node-propel/propel-express.git";
+var spaURL = "git@github.com:node-propel/propel-spa.git";
+var current_path = path.resolve(process.cwd());
 
+function express(name) {
+  /**
+   * download the app
+   */
+  console.log('Downloading propel-express application');
+  var cmd = 'git clone --depth=1 --branch=master ' + expressURL + ' ' + name + '; rm -rf ' + name + '/.git';
 
-function load(program, _, path, Generator) {
-  function loadApp() {
-    return require(path.join(process.cwd(), 'config', 'application.js'));
-  }
-
-  program
-    .usage('cmd [options]');
-
-  program
-    .command('new [NAME]')
-    .description('Create a propel application')
-    .action(function(name) {
-      if (!_.isEmpty(name)) {
-        new Generator(name);
-      }
-    });
-
-  program
-    .command('start')
-    .description('Start your propel application')
-    .option("--prod")
-    .option("--port <port>")
-    .on('--help', function() {
-      console.log('  HELP');
-      console.log('  ----');
-      console.log('    start --prod          Run application in production mode. Starts static file server serving out of `build` directory');
-      console.log('    start --port <port>   Start application with given port. Defaults to application.js configuration"');
-      console.log('');
-    })
-    .action(function(args) {
-      loadApp().run({ production: args.prod || false, port: _.parseInt(args.port) });
-    });
-
-  program
-    .command('build')
-    .description('Build your project')
-    .action(function() {
-      loadApp().build();
-    });
-
-  program
-    .command('clean')
-    .description('Clean your .tmp and build directories')
-    .action(function() {
-      loadApp().clean();
-    });
-
-  program
-    .command('pkg')
-    .description('Package the build up for distribution')
-    .option("-n --non-interactive")
-    .option("-c, --config-file <config-file>")
-    .option("-p, --properties [properties...]")
-    .on('--help', function() {
-      console.log('  HELP');
-      console.log('  ----');
-      console.log('    pkg -n                 Disable interactive terminal session. Flag to allow below options');
-      console.log('    pkg -c <config-file>   Read from config file. Defaults to application.js configuration');
-      console.log('    pkg -p [properties...] Indicate properties to read from config file (comma separated with no spaces)\n' +
-                  '                           used to create final tar ball. Defaults to application.js configuration');
-      console.log('');
-      console.log('    If no options are provided interactive session will start');
-      console.log('');
-    })
-    .action(function(args) {
-      var files = !_.isEmpty(args.properties) && _.isString(args.properties) ? args.properties.split(',') : '';
-      loadApp().pkg({ configFile: args.configFile, fileProps: files, interactive: args.nonInteractive !== true });
-    });
-
-  program
-    .command('bump')
-    .description('Bump or reset version numbers for specified files')
-    .option('-n, --non-interactive')
-    .option("-f, --files [version-files...]")
-    .option("-t, --type <bump-type>")
-    .option("-c, --custom <custom>")
-    .on('--help', function() {
-      console.log('  HELP');
-      console.log('  ----');
-      console.log('    bump -n                    Disable interactive terminal session. Flag to allow below options');
-      console.log('    bump -f [version-files...] Supply list of files (comma separated string with no spaces) to apply version bump.\n' +
-                  '                               Defaults to application.js configuration');
-      console.log('    bump -t <bump-type>        Bump files to specified bump type. Options: major, minor, patch. Defaults to application.js configuration');
-      console.log('    bump -c <custom>           Use custom version number to apply to all given files');
-      console.log('');
-      console.log('    If no options are provided interactive session will start    ');
-      console.log('');
-    })
-    .action(function(args) {
-      var files = !_.isEmpty(args.files) && _.isString(args.files) ? args.files.split(',') : '';
-      loadApp().bump({ files: files, type: args.type, custom: args.custom, interactive: args.nonInteractive !== true});
-    });
-
-
-  return program;
+  shell.exec(cmd);
+  shell.exec('echo "{ type: \'express\' }" > ' + path.join(current_path, name, '.propelrc'));
 }
+
+function spa(name) {
+  /**
+   * download the app
+   */
+  console.log('Downloading propel-spa application');
+  var cmd = 'git clone --depth=1 --branch=master ' + spaURL + ' ' + name + '; rm -rf ' + name + '/.git';
+
+  shell.exec(cmd);
+  shell.exec('echo "{ type: \'spa\' }" > ' + path.join(current_path, name, '.propelrc'));
+}
+
+function modifyPKG(name) {
+  /**
+   * modify the package.json with name of the app
+   */
+  var pkg = require(path.join(current_path, name, 'package.json'));
+  if (name === '.') {
+    pkg.name = path.basename(path.resolve(process.cwd()));
+  }
+  else {
+    pkg.name = name;
+  }
+  var data = JSON.stringify(pkg, null, 2) + '\n';
+  fs.writeFileSync(path.join(current_path, name, 'package.json'), data, 'utf-8');
+}
+
+program
+  .version(version);
+
+program
+  .usage('express [name]')
+  .command('express [name]')
+  .description('Initialize a propel-express application')
+  .action(function(name) {
+    if (name) {
+      express(name);
+      modifyPKG(name);
+    }
+  });
+
+program
+  .usage('spa [name]')
+  .command('spa [name]')
+  .description('Initialize a propel-spa application')
+  .action(function(name) {
+    if (name) {
+      spa(name);
+      modifyPKG(name);
+    }
+  });
+
+module.exports = program;
